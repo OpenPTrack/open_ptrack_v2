7255fe73292bb6cf65c0ba154792ae11952db415
temp <temp@temp>
2018-11-14 21:40:56 +0000

    bug fixed

--- recognition/scripts/face_saver.py
+++ recognition/scripts/face_saver.py
@@ -25,10 +25,10 @@ class FaceSaverNode:
 
 		for sensor_name in sensor_names:
 			try:
-				rospy.client.wait_for_message('/%s/rgb/image' % sensor_name, Image, 1.0)
-				image_sub = message_filters.Subscriber('/%s/rgb/image' % sensor_name, Image)
+				rospy.client.wait_for_message('/%s/hd/image_color' % sensor_name, Image, 1.0)
+				image_sub = message_filters.Subscriber('/%s/hd/image_color' % sensor_name, Image)
 			except:
-				image_sub = message_filters.Subscriber('/%s/rgb/image/compressed' % sensor_name, CompressedImage)
+				image_sub = message_filters.Subscriber('/%s/hd/image_color/compressed' % sensor_name, CompressedImage)
 
 			self.subscribers = [
 				image_sub,
