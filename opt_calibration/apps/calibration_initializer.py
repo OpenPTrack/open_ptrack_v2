47810a8a015c5be1dfa0455f003936ff425c42fa
MikoyChinese <emailmikoy@163.com>
2019-07-25 18:39:03 +0800

    Fix some error.

--- opt_calibration/apps/calibration_initializer.py
+++ opt_calibration/apps/calibration_initializer.py
@@ -48,7 +48,7 @@ from opt_msgs.srv import *
 class CalibrationInitializer :
 
   def __init__(self) :
-    
+
     network = rospy.get_param('~network')
     self.checkerboard = rospy.get_param('~checkerboard')
     self.file_name = rospy.get_param('~network_calibration_launch_file')
@@ -64,17 +64,17 @@ class CalibrationInitializer :
       self.sensor_map[pc] = sensors
       for sensor_item in sensors:
         self.sensor_list = self.sensor_list + [sensor_item]
-        
+
     self.session_id = rospy.Time.now().secs
-        
-  
+
+
   def createMaster(self) :
-    
+
     file = open(self.file_name, 'w')
     file.write('<?xml version="1.0"?>\n')
     file.write('<!-- SESSION ID: ' + str(self.session_id) + ' -->\n')
     file.write('<launch>\n\n')
-  
+
     # Calibration parameters
     file.write('  <!-- Calibration parameters -->\n')
     file.write('  <arg name="lock_world_frame" default="false" />\n')
@@ -82,11 +82,11 @@ class CalibrationInitializer :
     file.write('    <arg name="fixed_sensor_id" />\n')
     file.write('    <rosparam command="load" file="$(find opt_calibration)/conf/camera_poses.yaml" />\n')
     file.write('  </group>\n\n')
-  
+
     # Network parameters
     file.write('  <!-- Network parameters -->\n')
     file.write('  <arg name="num_sensors"   default="' + str(len(self.sensor_list)) + '" />\n\n')
-    
+
     index = 0
     for sensor in self.sensor_list:
 #       if sensor['type'] == 'sr4500':
@@ -97,38 +97,38 @@ class CalibrationInitializer :
       file.write('  <arg name="sensor_' + str(index) + '_id"   default="' + sensor['id']  + '" />\n')
       file.write('  <arg name="sensor_' + str(index) + '_name" default="$(arg sensor_' + str(index) + '_id)" />\n\n')
       index = index + 1
-      
+
     # Checkerboard parameters
     file.write('  <arg name="rows"          default="' + str(self.checkerboard['rows']) + '" />\n')
     file.write('  <arg name="cols"          default="' + str(self.checkerboard['cols']) + '" />\n')
     file.write('  <arg name="cell_width"    default="' + str(self.checkerboard['cell_width']) + '" />\n')
     file.write('  <arg name="cell_height"   default="' + str(self.checkerboard['cell_height']) + '" />\n\n')
-  
+
     # Rviz and calibration node
     file.write('  <!-- Opening Rviz for visualization -->\n')
     file.write('  <node name="rviz" pkg="rviz" type="rviz" args="-d $(find opt_calibration)/conf/opt_calibration.rviz" />\n\n')
-    
+
     file.write('  <!-- Plot calibration status -->\n')
     file.write('  <node name="opt_calibration_status_plot" pkg="opt_calibration" type="status_plot.py" output="screen">\n')
     file.write('    <remap from="~calibration_status" to="/opt_calibration/status" />\n')
     file.write('  </node>\n\n')
-    
+
     file.write('  <!-- Launching calibration -->\n')
     file.write('  <node pkg="opt_calibration" type="opt_calibration" name="opt_calibration" output="screen">\n\n')
-    
+
     file.write('    <param unless="$(arg lock_world_frame)" name="world_computation" value="last_checkerboard" />\n')
     file.write('    <param     if="$(arg lock_world_frame)" name="world_computation" value="update" />\n')
     file.write('    <param     if="$(arg lock_world_frame)" name="fixed_sensor/name" value="/$(arg fixed_sensor_id)" />\n\n')
-    
+
     file.write('    <param name="num_sensors"           value="$(arg num_sensors)" />\n\n')
     file.write('    <param name="rows"                  value="$(arg rows)" />\n')
     file.write('    <param name="cols"                  value="$(arg cols)" />\n')
     file.write('    <param name="cell_width"            value="$(arg cell_width)" />\n')
     file.write('    <param name="cell_height"           value="$(arg cell_height)" />\n\n')
-  
+
     #if (calibration_with_serials)??
     #  file.write('    <param name="calibration_with_serials" value="true" />\n\n')
-    
+
     index = 0
     for sensor in self.sensor_list:
       file.write('    <param name="sensor_' + str(index) + '/name"         value="/$(arg sensor_' + str(index) + '_name)" />\n')
@@ -159,10 +159,10 @@ class CalibrationInitializer :
       else:
         rospy.logfatal('Sensor type "' + sensor['type'] + '" not supported yet!');
       index = index + 1
-  
+
     file.write('  </node>\n\n')
     file.write('</launch>\n')
-    
+
     file.close()
 
     # Create launch file for defining user reference frame:
@@ -170,15 +170,15 @@ class CalibrationInitializer :
     file.write('<?xml version="1.0"?>\n')
     file.write('<!-- SESSION ID: ' + str(self.session_id) + ' -->\n')
     file.write('<launch>\n\n')
-  
+
     # Calibration parameters
     file.write('  <!-- Calibration parameters -->\n')
     file.write('  <rosparam command="load" file="$(find opt_calibration)/conf/camera_poses.yaml" />\n\n')
-    
+
     # Network parameters
     file.write('  <!-- Network parameters -->\n')
     file.write('  <arg name="num_sensors"   default="' + str(len(self.sensor_list)) + '" />\n\n')
-    
+
     index = 0
     for sensor in self.sensor_list:
 #       if sensor['type'] == 'sr4500':
@@ -189,15 +189,15 @@ class CalibrationInitializer :
       file.write('  <arg name="sensor_' + str(index) + '_id"   default="' + sensor['id']  + '" />\n')
       file.write('  <arg name="sensor_' + str(index) + '_name" default="$(arg sensor_' + str(index) + '_id)" />\n\n')
       index = index + 1
-      
+
     # Calibration node
     file.write('  <!-- Launching calibration -->\n')
     file.write('  <node pkg="opt_calibration" type="opt_define_reference_frame" name="opt_define_reference_frame" output="screen">\n')
-    
+
     file.write('    <rosparam command="load" file="$(find opt_calibration)/conf/camera_network.yaml" />\n\n')
-    
+
     file.write('    <param name="num_sensors"           value="$(arg num_sensors)" />\n\n')
-    
+
     index = 0
     for sensor in self.sensor_list:
       file.write('    <param name="sensor_' + str(index) + '/name"         value="/$(arg sensor_' + str(index) + '_name)" />\n')
@@ -228,29 +228,29 @@ class CalibrationInitializer :
       else:
         rospy.logfatal('Sensor type "' + sensor['type'] + '" not supported yet!');
       index = index + 1
-  
+
     file.write('  </node>\n\n')
     file.write('</launch>\n')
-    
+
     file.close()
-    
+
   def fileName(self) :
     return self.file_name
 
   def frameFileName(self) :
     return self.frame_file_name
-    
+
   def __invokeService(self, local_service_name) :
-    
+
     # For each pc
     for pc in self.sensor_map:
       service_name = pc + '/' + local_service_name
       rospy.loginfo('Waiting for ' + service_name + ' service...')
       rospy.wait_for_service(service_name)
-      
+
       # For each sensor
       for sensor_item in self.sensor_map[pc]:
-        
+
         # Create an OPTSensor message
         sensor_msg = OPTSensorRequest()
         sensor_msg.session_id = self.session_id
@@ -258,11 +258,11 @@ class CalibrationInitializer :
         if sensor_item['type'] == 'kinect1':
           sensor_msg.type = OPTSensorRequest.TYPE_KINECT1
           if 'serial' in sensor_item:
-            sensor_msg.serial = sensor_item['serial']   
+            sensor_msg.serial = sensor_item['serial']
         elif sensor_item['type'] == 'kinect2':
           sensor_msg.type = OPTSensorRequest.TYPE_KINECT2
           if 'serial' in sensor_item:
-            sensor_msg.serial = sensor_item['serial']  
+            sensor_msg.serial = sensor_item['serial']
         elif sensor_item['type'] == 'sr4500':
           sensor_msg.type = OPTSensorRequest.TYPE_SR4500
           sensor_msg.ip = sensor_item['ip']
@@ -277,7 +277,7 @@ class CalibrationInitializer :
         elif sensor_item['type'] == 'realsense':
           sensor_msg.type = OPTSensorRequest.TYPE_REALSENSE
           if 'serial' in sensor_item:
-            sensor_msg.id_num = sensor_item['serial']
+            sensor_msg.serial = sensor_item['serial']
 
         if 'people_detector' in sensor_item:
           if sensor_item['people_detector'] == 'yolo':
@@ -299,36 +299,36 @@ class CalibrationInitializer :
             rospy.logerr('[' + pc + '] ' + response.message);
         except rospy.ServiceException, e:
           rospy.logerr("Service call failed: %s"%e)
-  
+
   def createSensorLaunch(self) :
     self.__invokeService('create_sensor_launch')
-          
+
   def createDetectorLaunch(self) :
     self.__invokeService('create_detector_launch')
 
   def createTrackerLaunch(self) :
     self.__invokeService('create_tracker_launch')
-    
+
 
 if __name__ == '__main__' :
-  
+
   rospy.init_node('calibration_initializer')
-    
+
   try:
-    
+
     initializer = CalibrationInitializer()
-    
+
     initializer.createMaster()
     rospy.loginfo(initializer.fileName() + ' created!');
     rospy.loginfo(initializer.frameFileName() + ' created!');
-    
+
     initializer.createSensorLaunch()
     initializer.createDetectorLaunch()
     initializer.createTrackerLaunch()
-    
+
     rospy.loginfo('Initialization completed. Press [ctrl+c] to exit.')
-    
+
     rospy.spin()
-    
+
   except rospy.ROSInterruptException:
     pass
--- opt_calibration/apps/listener.py
+++ opt_calibration/apps/listener.py
@@ -50,11 +50,11 @@ class Listener :
     self.sensor_launchers_dir = rospy.get_param('~sensor_launchers_dir')
     if self.sensor_launchers_dir[len(self.sensor_launchers_dir) - 1] != '/':
       self.sensor_launchers_dir = self.sensor_launchers_dir + '/'
-      
+
     self.detector_launchers_dir = rospy.get_param('~detector_launchers_dir')
     if self.detector_launchers_dir[len(self.detector_launchers_dir) - 1] != '/':
       self.detector_launchers_dir = self.detector_launchers_dir + '/'
-      
+
     self.camera_poses_dir = rospy.get_param('~camera_poses_dir')
     if self.camera_poses_dir[len(self.camera_poses_dir) - 1] != '/':
       self.camera_poses_dir = self.camera_poses_dir + '/'
@@ -62,12 +62,12 @@ class Listener :
     self.tracker_launchers_dir = rospy.get_param('~tracker_launchers_dir')
     if self.tracker_launchers_dir[len(self.tracker_launchers_dir) - 1] != '/':
       self.tracker_launchers_dir = self.tracker_launchers_dir + '/'
-    
+
     self.create_sensor_launch_srv = rospy.Service('create_sensor_launch', OPTSensor, self.handle_create_sensor_launch)
     self.create_detector_launch_srv = rospy.Service('create_detector_launch', OPTSensor, self.handle_create_detector_launch)
     self.create_tracking_launch_srv = rospy.Service('create_tracker_launch', OPTSensor, self.handle_create_tracker_launch)
     self.create_camera_poses_srv = rospy.Service('create_camera_poses', OPTTransform, self.handle_create_camera_poses)
-    
+
   def handle_create_tracker_launch(self, request):
     file_name = self.tracker_launchers_dir + 'tracking_node.launch'
     file = open(file_name, 'w')
@@ -112,20 +112,20 @@ class Listener :
     file.write('</launch>\n')
     file.close();
     rospy.loginfo(file_name + ' created!');
-    
+
     return (OPTSensorResponse.STATUS_OK, file_name + ' created!')
 
   def handle_create_sensor_launch(self, request):
-    
+
     file_name = self.sensor_launchers_dir + 'sensor_' + request.id + '.launch'
     file = open(file_name, 'w')
     file.write('<?xml version="1.0"?>\n')
     file.write('<!-- SESSION ID: ' + str(request.session_id) + ' -->\n')
     file.write('<!-- SERIAL: ' + str(request.serial) + ' -->\n')
     file.write('<launch>\n\n')
-    
+
     file.write('  <!-- Sensor parameters -->\n')
-    
+
     # if request.type == OPTSensorRequest.TYPE_SR4500:
     #   file.write('  <arg name="camera_id"   default="' + request.id + '" />\n')
     #   if request.ip == '':
@@ -133,24 +133,24 @@ class Listener :
     #     rospy.logerr('Missing "ip" field for the SR4500 sensor!')
     #     return (OPTSensorResponse.STATUS_ERROR, 'Missing "ip" field!')
     #   file.write('  <arg name="device_ip"   default="' + request.ip + '" />\n\n')
-      
+
     #   file.write('  <!-- Launch sensor -->\n')
     #   file.write('  <include file="$(find swissranger_camera)/launch/sr_eth.launch">\n')
     #   file.write('    <arg name="camera_id" value="$(arg camera_id)" />\n')
     #   file.write('    <arg name="device_ip" value="$(arg device_ip)" />\n')
     #   file.write('  </include>\n\n')
-      
+
     #   file.write('  <include file="$(find swissranger_camera)/launch/publisher_for_calibration.launch">\n')
     #   file.write('    <arg name="camera_id"       value="$(arg camera_id)" />\n')
     #   file.write('    <arg name="camera_info_url" value="file://$(find opt_calibration)/camera_info/$(arg camera_id).yaml" />\n')
     #   file.write('  </include>\n\n')
-      
+
     if request.type == OPTSensorRequest.TYPE_KINECT1:
       file.write('  <arg name="sensor_id"     default="' + request.id + '" />\n')
       if request.serial != '':
         file.write('  <arg name="sensor_serial" default="' + request.serial + '" />\n')
       file.write('\n')
-      
+
       file.write('  <!-- Launch sensor -->\n')
       file.write('  <include file="$(find detection)/launch/$(env KINECT_DRIVER).launch">\n')
       if request.serial != '':
@@ -160,17 +160,17 @@ class Listener :
         file.write('    <arg name="rgb_camera_info_url" value="file://$(find opt_calibration)/camera_info/rgb_$(arg sensor_id).yaml" />\n')
       file.write('    <arg name="camera"              value="$(arg sensor_id)" />\n')
       file.write('  </include>\n\n')
-      
+
       file.write('  <!-- Publish a further transform -->\n')
       file.write('  <node pkg="tf" type="static_transform_publisher" name="$(arg sensor_id)_broadcaster" args="-0.045 0 0 1.57079 -1.57079 0 /$(arg sensor_id) /$(arg sensor_id)_link  100" />\n\n')
-    
+
     elif request.type == OPTSensorRequest.TYPE_STEREO_PG:
       file.write('  <arg name="camera_serial_left"  default="' + request.serial_left + '" />\n')
       file.write('  <arg name="camera_serial_right" default="' + request.serial_right + '" />\n')
       file.write('  <arg name="stereo_name"         default="' + request.id + '" />\n')
       file.write('  <arg name="conf_file_left"      default="$(find opt_calibration)/conf/pg_default.yaml" />\n')
       file.write('  <arg name="conf_file_right"     default="$(find opt_calibration)/conf/pg_default.yaml" />\n\n')
-      
+
       file.write('  <!-- Launch driver -->\n')
       file.write('  <include file="$(find opt_calibration)/launch/stereo_pg.launch">\n')
       file.write('    <arg name="stereo_name"         value="$(arg stereo_name)" />\n')
@@ -182,13 +182,13 @@ class Listener :
       file.write('  <!-- Launch stereo processing -->\n')
       file.write('  <include file="$(find opt_calibration)/launch/stereo_processing.launch">\n')
       file.write('    <arg name="stereo_namespace" value="$(arg stereo_name)" />\n')
-      file.write('  </include>\n\n')  
+      file.write('  </include>\n\n')
     elif request.type == OPTSensorRequest.TYPE_KINECT2:
       file.write('  <arg name="sensor_name"     default="' + request.id + '" />\n')
       if request.serial != '':
         file.write('  <arg name="sensor_id" default="' + request.serial + '" />\n')
       file.write('\n')
-      
+
       file.write('  <!-- Launch sensor -->\n')
       file.write('  <include file="$(find kinect2_bridge)/launch/kinect2_bridge_ir.launch">\n')
       if request.serial != '':
@@ -198,10 +198,10 @@ class Listener :
       #  file.write('    <arg name="rgb_camera_info_url" value="file://$(find opt_calibration)/camera_info/rgb_$(arg sensor_id).yaml" />\n')
       file.write('    <arg name="sensor_name"         value="$(arg sensor_name)" />\n')
       file.write('    <arg name="publish_frame"       value="false" />\n')
-      file.write('  </include>\n\n') 
+      file.write('  </include>\n\n')
 
       file.write('  <!-- Publish a further transform -->\n')
-      file.write('  <node pkg="tf" type="static_transform_publisher" name="$(arg sensor_name)_broadcaster" args="0 0 0 1.57079 -1.57079 0 /$(arg sensor_name) /$(arg sensor_name)_link  100" />\n\n')     
+      file.write('  <node pkg="tf" type="static_transform_publisher" name="$(arg sensor_name)_broadcaster" args="0 0 0 1.57079 -1.57079 0 /$(arg sensor_name) /$(arg sensor_name)_link  100" />\n\n')
     elif request.type == OPTSensorRequest.TYPE_ZED:
       file.write('  <arg name="sensor_name"     default="' + request.id + '" />\n')
       if request.id_num != '':
@@ -213,14 +213,14 @@ class Listener :
       if request.id_num != '':
         file.write('    <arg name="zed_id"           value="$(arg sensor_id)" />\n')
       # file.write('    <arg name="sensor_name"         value="$(arg sensor_name)" />\n')
-      file.write('  </include>\n\n') 
+      file.write('  </include>\n\n')
       file.write('  </group>\n\n')
     elif request.type == OPTSensorRequest.TYPE_REALSENSE:
       file.write('  <arg name="sensor_name"     default="' + request.id + '" />\n')
       if request.serial != '':
         file.write('  <arg name="sensor_id" default="' + request.serial + '" />\n')
       file.write('\n')
-      
+
       file.write('  <!-- Launch sensor -->\n')
       # file.write('  <include file="$(find kinect2_bridge)/launch/kinect2_bridge_ir.launch">\n')
       file.write('<include file="$(find realsense2_camera)/launch/rs_rgbd.launch">\n')
@@ -231,18 +231,18 @@ class Listener :
       #  file.write('    <arg name="rgb_camera_info_url" value="file://$(find opt_calibration)/camera_info/rgb_$(arg sensor_id).yaml" />\n')
       file.write('      <arg name="camera"         value="$(arg sensor_name)" />\n')
       # file.write('    <arg name="publish_frame"       value="true" />\n')
-      file.write('  </include>\n\n') 
+      file.write('  </include>\n\n')
 
       # file.write('  <!-- Publish a further transform -->\n')
-      # file.write('  <node pkg="tf" type="static_transform_publisher" name="$(arg sensor_name)_broadcaster" args="0 0 0 1.57079 -1.57079 0 /$(arg sensor_name) /$(arg sensor_name)_link  100" />\n\n')     
+      # file.write('  <node pkg="tf" type="static_transform_publisher" name="$(arg sensor_name)_broadcaster" args="0 0 0 1.57079 -1.57079 0 /$(arg sensor_name) /$(arg sensor_name)_link  100" />\n\n')
 
     file.write('</launch>\n')
     file.close();
     rospy.loginfo(file_name + ' created!');
 
-    
+
     return (OPTSensorResponse.STATUS_OK, file_name + ' created!')
-    
+
   def handle_create_detector_launch(self, request) :
 
     file_name = self.detector_launchers_dir + 'detection_node_' + request.id + '.launch'
@@ -262,7 +262,7 @@ class Listener :
       file.write('  <arg name="enable_object" default="true" />\n')
     else:
       file.write('  <arg name="enable_object" default="false" />\n')
-    
+
     # if request.type == OPTSensorRequest.TYPE_SR4500:
     #   file.write('  <arg name="camera_id"       default="' + request.id + '" />\n')
     #   if request.ip == '':
@@ -271,7 +271,7 @@ class Listener :
     #     return (OPTSensorResponse.STATUS_ERROR, 'Missing "ip" field!')
     #   file.write('  <arg name="device_ip"       default="' + request.ip + '" />\n')
     #   file.write('  <arg name="camera_info_url" default="file://$(find opt_calibration)/camera_info/$(arg camera_id).yaml" />\n\n')
-      
+
     #   file.write('  <!-- Detection node -->\n')
     #   file.write('  <include file="$(find detection)/launch/detector_sr4500.launch">\n')
     #   file.write('    <arg name="camera_id"               value="$(arg camera_id)" />\n')
@@ -279,12 +279,12 @@ class Listener :
     #   file.write('    <arg name="ground_from_calibration" value="true" />\n')
     #   file.write('    <arg name="camera_info_url"         value="$(arg camera_info_url)" />\n')
     #   file.write('  </include>\n\n')
-    
+
     if request.type == OPTSensorRequest.TYPE_KINECT1:
       if request.serial != '':
         file.write('  <arg name="device_id"   default="' + request.serial + '" />\n')
       file.write('  <arg name="camera_name" default="' + request.id + '" />\n\n')
-      
+
       file.write('  <!-- Detection node -->\n')
       file.write('  <include file="$(find detection)/launch/detector_kinect1.launch">\n')
       if request.serial != '':
@@ -295,14 +295,14 @@ class Listener :
       file.write('    <arg name="camera_name"             value="$(arg camera_name)" />\n')
       file.write('    <arg name="ground_from_calibration" value="true" />\n')
       file.write('  </include>\n\n')
-      
+
     elif request.type == OPTSensorRequest.TYPE_STEREO_PG:
       file.write('  <arg name="camera_serial_left"  default="' + request.serial_left + '" />\n')
       file.write('  <arg name="camera_serial_right" default="' + request.serial_right + '" />\n')
       file.write('  <arg name="stereo_name"         default="' + request.id + '" />\n')
       file.write('  <arg name="conf_file_left"      default="$(find opt_calibration)/conf/pg_default.yaml" />\n')
       file.write('  <arg name="conf_file_right"     default="$(find opt_calibration)/conf/pg_default.yaml" />\n\n')
-     
+
       file.write('  <!-- Detection node -->\n')
       file.write('  <include file="$(find detection)/launch/detector_stereo_pg.launch">\n')
       file.write('    <arg name="stereo_name"             value="$(arg stereo_name)" />\n')
@@ -361,7 +361,7 @@ class Listener :
       file.write('      <arg name="ground_from_calibration" value="true" />\n')
       file.write('    </include>\n\n')
       file.write('  </group>\n\n')
-      
+
       file.write('  <!-- Object Detection node -->\n')
       file.write('  <group if="$(arg enable_object)">\n')
       file.write('    <include file="$(find yolo_detector)/launch/detect_yolo_object_kinect_network.launch">\n')
@@ -369,7 +369,7 @@ class Listener :
         file.write('      <arg name="sensor_id"               value="$(arg sensor_id)" />\n')
       file.write('      <arg name="sensor_name"             value="$(arg sensor_name)" />\n')
       file.write('    </include>\n\n')
-      file.write('  </group>\n\n') 
+      file.write('  </group>\n\n')
 
     elif request.type == OPTSensorRequest.TYPE_ZED:
       if request.id_num != '':
@@ -410,13 +410,13 @@ class Listener :
         file.write('                  <arg name="ground_from_calibration" value="true" />\n')
         file.write('    </include>\n')
       file.write('  </group>\n\n')
-    
+
     elif request.type == OPTSensorRequest.TYPE_REALSENSE:
       if request.serial != '':
         file.write('  <arg name="sensor_id"   default="' + request.serial + '" />\n')
       file.write('  <arg name="sensor_name" default="' + request.id + '" />\n\n')
-      file.write('  <arg name="width" default="424" />\n')
-      file.write('  <arg name="height" default="240" />\n\n')
+      file.write('  <arg name="width" default="640" />\n')
+      file.write('  <arg name="height" default="480" />\n\n')
       file.write('  <!-- true  = Munaro Based OPT Detection -->\n')
       file.write('  <!-- false = YOLO Based Detection (Must Have YOLO installed to use)-->\n')
       if request.people_detector_type == OPTSensorRequest.PEOPLE_DETECTOR_YOLO_BASED:
@@ -463,53 +463,51 @@ class Listener :
       file.write('  <!-- Skeleton Detection node -->\n')
       file.write('  <group if="$(arg enable_pose)">\n')
       file.write('    <include file="$(find detection)/launch/skeleton_detector_realsense.launch">\n')
-      if request.serial != '':
-        file.write('      <arg name="serial_no"               value="$(arg sensor_id)" />\n')
       file.write('      <arg name="sensor_name"             value="$(arg sensor_name)" />\n')
       file.write('      <arg name="ground_from_calibration" value="true" />\n')
       file.write('    </include>\n\n')
       file.write('  </group>\n\n')
-      
+
       file.write('  <!-- Object Detection node -->\n')
       file.write('  <group if="$(arg enable_object)">\n')
       file.write('    <include file="$(find yolo_detector)/launch/detect_yolo_object_realsense_network.launch">\n')
       file.write('      <arg name="sensor_name"             value="$(arg sensor_name)" />\n')
       file.write('    </include>\n\n')
-      file.write('  </group>\n\n') 
+      file.write('  </group>\n\n')
 
     file.write('</launch>\n')
     file.close();
     rospy.loginfo(file_name + ' created!');
-  
+
     return (OPTSensorResponse.STATUS_OK, file_name + ' created!')
-  
+
   def handle_create_camera_poses(self, request) :
-    
+
     file_name = self.camera_poses_dir + 'camera_poses.txt'
     file = open(file_name, 'w')
     file.write('# Auto-generated file.\n')
     file.write('# CALIBRATION ID: ' + str(request.calibration_id) + '\n')
-    
+
     data = zip(request.child_id, request.transform)
     for item in data:
       t = item[1].translation
       r = item[1].rotation
       file.write(item[0] + ': ' + str(t.x) + ' ' + str(t.y) + ' ' + str(t.z) + ' ')
       file.write(str(r.x) + ' ' + str(r.y) + ' ' + str(r.z) + ' ' + str(r.w) + '\n')
-    
+
     file.close()
     rospy.loginfo(file_name + ' created!');
-    
+
     return (OPTTransformResponse.STATUS_OK, file_name + ' created!')
-    
+
 if __name__ == '__main__' :
-  
+
   rospy.init_node('listener')
-  
+
   try:
-    
+
     listener = Listener()
     rospy.spin()
-    
+
   except rospy.ROSInterruptException:
     pass
