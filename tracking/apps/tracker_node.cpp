c9761e2718fc11120c2bca628cd9e951daf7b22a
bsubbaraman <bms12014@mymail.pomona.edu>
2019-10-02 17:33:40 -0700

    Update tracker_node.cpp
    for single_camera, need to check sensor_type and apply correct fixed rotation/inv fixed rotation

--- tracking/apps/tracker_node.cpp
+++ tracking/apps/tracker_node.cpp
@@ -776,10 +776,22 @@ main(int argc, char** argv)
   // If extrinsic calibration is not available:
   if (!extrinsic_calibration)
   { // Set fixed transformation from rgb frame and base_link
+    std::string sensor_type;
+    nh.param("sensor_type", sensor_type, std::string());
+    tf::Quaternion fixed_rotation; // camera_rgb_optical_frame -> world
+    tf::Quaternion inv_fixed_rotation; // world -> camera_rgb_optical_frame
+    if (sensor_type == "zed")
+    {
+      fixed_rotation = tf::Quaternion(0.0,0.0,0.0,1.0);
+      inv_fixed_rotation = tf::Quaternion(0.0,0.0,0.0,1.0);
+    }
+    else
+    {
+      fixed_rotation = tf::Quaternion(-0.5,0.5,-0.5, -0.5);
+      inv_fixed_rotation = tf::Quaternion(-0.5,0.5,-0.5,0.5);
+    }
     tf::Vector3 fixed_translation(0, 0, 0);                  // camera_rgb_optical_frame -> world
-    tf::Quaternion fixed_rotation(0.0, 0.0, 0.0, 1.0);	// camera_rgb_optical_frame -> world
     tf::Vector3 inv_fixed_translation(0.0, 0.0, 0);			// world -> camera_rgb_optical_frame
-    tf::Quaternion inv_fixed_rotation(0.0, 0.0, 0.0, 1.0);	// world -> camera_rgb_optical_frame
     world_to_camera_frame_transform = tf::Transform(fixed_rotation, fixed_translation);
     camera_frame_to_world_transform = tf::Transform(inv_fixed_rotation, inv_fixed_translation);
   }
